ae136a7ad61e3227a3ea1bf866f5f8ded5bcceae
suryadharma5 <suryadharmasetiawan03@gmail.com>
2023-11-21 00:25:56 +0700

    added: budget form ui

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,10 +5,12 @@ import { useLoaderData } from 'react-router-dom';
 // components
 import Intro from '../components/Intro';
 import { toast } from 'react-toastify';
+import AddBudgetForm from '../components/AddBudgetForm';
 
 export function dashboardLoader() {
     const userName = fetchData("userName");
-    return { userName }
+    const budgets = fetchData("budgets")
+    return { userName, budgets }
 }
 
 export async function dashboardAction({ request }) {
@@ -24,12 +26,22 @@ export async function dashboardAction({ request }) {
 }
 
 function Dashboard() {
-    const { userName } = useLoaderData()
+    const { userName, budgets } = useLoaderData()
 
     return (
         <div>
             {userName ? (
-                <p>{userName}</p>
+                <div className='dashboard'>
+                    <h1>Welcome back, <span className='accent'>{userName}</span></h1>
+                    <div className="grid-sm">
+                        {/* {budgets ? (): ()} */}
+                        <div className="grid-lg">
+                            <div className="flex-lg">
+                                <AddBudgetForm />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             ) : (
                 <Intro />
             )}
