3b3b2228fbfbb8abbf9c86adc84a5f517d537bdc
suryadharma5 <suryadharmasetiawan03@gmail.com>
2023-11-30 00:09:48 +0700

    added: delete ui

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,18 +1,36 @@
 import React from 'react'
 import { formatCurrency, formatDate, getAllMatchingItems } from '../helpers'
+import { Link, useFetcher } from 'react-router-dom'
+import { TrashIcon } from '@heroicons/react/24/solid'
 
 const ExpenseItem = ({ expense }) => {
+    const fetcher = useFetcher()
     const budget = getAllMatchingItems({
         category: "budgets",
         key: "id",
         value: expense.budgetId
-    })
+    })[0]
+    // console.log(budget)
+
     return (
         <>
             <td>{expense.name}</td>
             <td>{formatCurrency(expense.amount)}</td>
             <td>{formatDate(expense.createdAt)}</td>
-            <td>{formatDate(expense.createdAt)}</td>
+            <td>
+                <Link to={`/budget/${budget.id}`} style={{ "--accent": budget.color }}>
+                    {budget.name}
+                </Link>
+            </td>
+            <td>
+                <fetcher.Form method='POST'>
+                    <input type="hidden" name='_action' value="deleteExpense" />
+                    <input type="hidden" name='expenseId' value={expense.id} />
+                    <button type='submit' className='btn btn--warning'>
+                        <TrashIcon width={20} />
+                    </button>
+                </fetcher.Form>
+            </td>
         </>
     )
 }
